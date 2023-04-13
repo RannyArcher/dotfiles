b8ff8e08cdb4aacefbf2477571c4d6e675d9800d
Ranny Archer <omranoon4@gmail.com>
2023-04-13 04:29:40 +0000

    fish: fix dbl!!2prevcmd earease keybinds

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,17 @@
 if status is-interactive
-  # vi completion keybindings
-  bind -M insert j 'commandline -P; and down-or-search; or commandline -i j'
-  bind -M insert k 'commandline -P; and up-or-search; or commandline -i k'
-  bind -M insert h 'commandline -P; and commandline -f backward-char; or commandline -i h'
-  bind -M insert l 'commandline -P; and commandline -f forward-char; or commandline -i l'
+
+  # replace double exclamative with last command
+  _dblExclamations2lastcmd
 
   # change cursor on different vi modes
   set -g fish_cursor_insert line
   set -g fish_cursor_default block
   set -g fish_cursor_visual underscore
 
-  # replace double exclamative with last command
-  _dblExclamations2lastcmd   
+  # vi completion keybindings
+  bind -M insert j 'commandline -P; and down-or-search; or commandline -i j'                
+  bind -M insert k 'commandline -P; and up-or-search; or commandline -i k'                  
+  bind -M insert h 'commandline -P; and commandline -f backward-char; or commandline -i h'
+  bind -M insert l 'commandline -P; and commandline -f forward-char; or commandline -i l'
+
 end
